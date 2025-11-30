8f77ad5927de9f11556d7b3344b244fbf129c311
Swan Htet Aung <137768986+swanhtetaung01@users.noreply.github.com>
2025-12-01 01:12:11 +0700

    Add constructors

--- src/main/java/org/javarevision/OOPs/Car/Car.java
+++ src/main/java/org/javarevision/OOPs/Car/Car.java
@@ -2,14 +2,50 @@ package org.javarevision.OOPs.Car;
 
 public class Car {
 
-  public String model;
-  public String color;
-  public int seats;
+    public String model;
+    public String color;
+    public int seats;
 
-  public void display(){
-      System.out.println("Model is : " + model);
-      System.out.println("Color is : " + color);
-      System.out.println("Seats is : " + seats);
-  }
+    public Car(){
+        model = "Test model";
+        color = "test Color";
+        seats = 4;
+        System.out.println("First constructor");
+    }
 
+    public Car(String model, String color, int seats){
+        this.model = model;
+        this.color = color;
+        this.seats = seats;
+    }
+
+    public void display(){
+        System.out.println("Model is : " + model);
+        System.out.println("Color is : " + color);
+        System.out.println("Seats is : " + seats);
+    }
+
+
+    /* ---- Methods VS constructors ------
+    * Constructors have no return type, whereas methods do have
+    * Constructors name should be same as class name, however that’s not
+      true in case of methods
+    * Java has a default constructor, but there is no such thing called default methods
+
+    * */
+
+
+
+    /*
+
+    ---- Rules for constructors -----
+
+    * Same name as classes
+    * Constructors don’t have a return type
+    * Declaring constructors in not mandatory
+    * There won’t be any default constructor created by java if you have
+      declared a constructor
+    * Constructors cannot be inherited
+
+    */
 }
--- src/main/java/org/javarevision/OOPs/Car/ClassesMain.java
+++ src/main/java/org/javarevision/OOPs/Car/ClassesMain.java
@@ -3,23 +3,9 @@ package org.javarevision.OOPs.Car;
 public class ClassesMain {
     public static void main(String[] args) {
 
-        Car ferrari = new Car();
-        ferrari.model = "Ferrari F430";
-        ferrari.seats = 4;
-        ferrari.color = "Red";
+        Car ferrari = new Car("Ferrari F430","Red",4);
         ferrari.display();
 
-        Car tesla = new Car();
-        tesla.model = "Model S";
-        tesla.seats = 4;
-        tesla.color = "Black";
-        tesla.display();
-
-        Car audi = new Car();
-        audi.model = "Audi SQ5";
-        audi.color = "Blue";
-        audi.seats = 4;
-        audi.display();
 
 
     }
