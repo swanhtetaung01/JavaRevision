c08a13d2b4fcd905e23fa5bd927e72436c0347c1
Swan Htet Aung <137768986+swanhtetaung01@users.noreply.github.com>
2025-12-02 23:14:07 +0700

    Add encapsulation example

---
+++ src/main/java/org/javarevision/OOPs/Polymorphism/encapsulation/A.java
@@ -0,0 +1,32 @@
+package org.javarevision.OOPs.Polymorphism.encapsulation;
+
+public class A {
+
+    private int var = 50;
+
+    public int getVar() {
+        return var;
+    }
+
+    public void setVar(int var) {
+        this.var = var;
+    }
+
+
+   /* //Getter
+    public int getVar(){
+        return var;
+    }
+
+    //Setter
+    public void setVar(int var){
+        if(var > 1000){
+            this.var = 0;
+        }
+        else {
+            this.var = var;
+        }
+    }*/
+
+
+}
---
+++ src/main/java/org/javarevision/OOPs/Polymorphism/encapsulation/EncapsulationMain.java
@@ -0,0 +1,15 @@
+package org.javarevision.OOPs.Polymorphism.encapsulation;
+
+public class EncapsulationMain {
+    public static void main(String[] args) {
+
+        A a = new A();
+        //a.var = 100;
+        System.out.println("Value in var : " + a.getVar());
+        a.setVar(100);
+        System.out.println("Value in var : " + a.getVar());
+
+        a.setVar(1500);
+        System.out.println("Value in var : " + a.getVar());
+    }
+}
