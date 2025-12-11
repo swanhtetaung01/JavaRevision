4330b698f04c89d69ed67e3d58dde0448c6bacb9
Swan Htet Aung <137768986+swanhtetaung01@users.noreply.github.com>
2025-12-11 21:31:59 +0700

    Update exception package

---
+++ src/main/java/org/javarevision/exceptions/methodstoprintexceptions/MethodToPrintException.java
@@ -0,0 +1,23 @@
+package org.javarevision.exceptions.methodstoprintexceptions;
+
+public class MethodToPrintException {
+
+    public static void main(String[] args) {
+        try {
+            System.out.println("Statement 1");
+            System.out.println(10 / 0);
+            System.out.println("Statement 3");
+        } catch (ArithmeticException e) {
+            e.printStackTrace(); //1
+
+
+            /*
+            System.out.println(e.toString());  //2
+
+            /*
+            System.out.println(e.getMessage());  //3
+            */
+        }
+        System.out.println("Statement 5");
+    }
+}
