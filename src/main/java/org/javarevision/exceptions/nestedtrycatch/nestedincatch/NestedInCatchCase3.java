4b5ba3271d3117e746e3e03b6d43e606fb49475b
Swan Htet Aung <137768986+swanhtetaung01@users.noreply.github.com>
2025-12-12 23:31:41 +0700

    Add nested try catch

---
+++ src/main/java/org/javarevision/exceptions/nestedtrycatch/nestedincatch/NestedInCatchCase3.java
@@ -0,0 +1,20 @@
+package org.javarevision.exceptions.nestedtrycatch.nestedincatch;
+
+public class NestedInCatchCase3 {
+    public static void main(String[] args) {
+
+        try {
+            System.out.println(10/0);
+        } catch (Exception e) {
+            System.out.println("statement 2");
+            try {
+                System.out.println(10/0);
+            } catch (Exception ex) {
+                System.out.println("statement 4");
+            }
+        } finally {
+            System.out.println("statement 5");
+        }
+    }
+
+}
