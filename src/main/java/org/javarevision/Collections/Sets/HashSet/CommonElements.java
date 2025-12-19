54cc8952262fb560d55c07824bf30859a1742a0b
Swan Htet Aung <137768986+swanhtetaung01@users.noreply.github.com>
2025-12-19 17:01:04 +0700

    Update HashSet Package

--- src/main/java/org/javarevision/Collections/Sets/HashSet/CommonElements.java
+++ src/main/java/org/javarevision/Collections/Sets/HashSet/CommonElements.java
@@ -7,10 +7,19 @@ import java.util.Set;
 public class CommonElements {
     public static void main(String[] args) {
 
-        Set c1 = new HashSet(Arrays.asList("Ram", "Rack", "Dhoni", "Harshad", "Modi", "Vijay", "Sonia"));
-        Set c2 = new HashSet(Arrays.asList("Ravi", "Faisal", "Prateek", "Aghishek", "Dhoni", "Harshad", "Ram", "Akash"));
-        Set c3 = new HashSet(Arrays.asList("Modi", "Vijay", "Dhoni", "Faisal", "Prateek", "Harshad", "Ram", "Ravi"));
+        HashSet<String> c1 = new HashSet(Arrays.asList("Ram", "Rack", "Dhoni", "Harshad", "Modi", "Vijay", "Sonia"));
+        HashSet<String> c2 = new HashSet(Arrays.asList("Ravi", "Faisal", "Prateek", "Aghishek", "Dhoni", "Harshad", "Ram", "Akash"));
+        HashSet<String> c3 = new HashSet(Arrays.asList("Modi", "Vijay", "Dhoni", "Faisal", "Prateek", "Harshad", "Ram", "Ravi"));
 
+        HashSet<String> result = commonElements(c1, c2, c3);
+        System.out.println("CommonElements are: " + result);
+    }
+    public static HashSet commonElements(HashSet<String> s1, HashSet<String> s2, HashSet<String> s3){
 
+        HashSet<String> result = new HashSet<String>(s1);
+        result.retainAll(s2);
+        result.retainAll(s3);
+        System.out.println(result);
+        return result;
     }
 }
