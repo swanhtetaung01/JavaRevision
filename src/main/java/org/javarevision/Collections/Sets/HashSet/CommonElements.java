a159aa8edc8fa384a90e2b9218c5225fdb62accf
Swan Htet Aung <137768986+swanhtetaung01@users.noreply.github.com>
2025-12-19 16:36:45 +0700

    Add HashSet package

---
+++ src/main/java/org/javarevision/Collections/Sets/HashSet/CommonElements.java
@@ -0,0 +1,16 @@
+package org.javarevision.Collections.Sets.HashSet;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class CommonElements {
+    public static void main(String[] args) {
+
+        Set c1 = new HashSet(Arrays.asList("Ram", "Rack", "Dhoni", "Harshad", "Modi", "Vijay", "Sonia"));
+        Set c2 = new HashSet(Arrays.asList("Ravi", "Faisal", "Prateek", "Aghishek", "Dhoni", "Harshad", "Ram", "Akash"));
+        Set c3 = new HashSet(Arrays.asList("Modi", "Vijay", "Dhoni", "Faisal", "Prateek", "Harshad", "Ram", "Ravi"));
+
+
+    }
+}
