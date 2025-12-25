8f28db1199c6dab0d450b2e1d8644775e459abc5
Swan Htet Aung <137768986+swanhtetaung01@users.noreply.github.com>
2025-12-25 22:38:03 +0700

    Add File Handling Package

---
+++ src/main/java/org/javarevision/FileHandlingUsingJavaIOapi/CreateFile.java
@@ -0,0 +1,17 @@
+package org.javarevision.FileHandlingUsingJavaIOapi;
+
+
+import java.io.File;
+
+public class CreateFile {
+    public static void main(String[] args) {
+        File textFile = new File("abc.txt");
+        System.out.println(textFile.exists());
+
+        String currentDirectory = System.getProperty("user.dir");
+        System.out.println("The current working directory is : " + currentDirectory);
+
+        File srcFolder = new File("src");
+        System.out.println(srcFolder.exists());
+    }
+}
