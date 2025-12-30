ffae782d4c31247c2698c29f177cb11628b3b742
Swan Htet Aung <137768986+swanhtetaung01@users.noreply.github.com>
2025-12-31 00:57:18 +0700

    Add transient keyword demo

---
+++ src/main/java/org/javarevision/FileHandlingUsingJavaIOapi/SerializationAndDeserialization/TransientDemo.java
@@ -0,0 +1,32 @@
+package org.javarevision.FileHandlingUsingJavaIOapi.SerializationAndDeserialization;
+
+import java.io.*;
+
+class Car1 implements Serializable{
+    String name;
+    transient String color;
+
+    public Car1(String name, String color) {
+        this.name = name;
+        this.color = color;
+    }
+}
+
+public class TransientDemo {
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        Car1 car = new Car1("Ferrari", "Red");
+
+        //Serialization
+        ObjectOutputStream objectOutputStream =
+                new ObjectOutputStream(new FileOutputStream("abc.txt"));
+        objectOutputStream.writeObject(car);
+
+        //Deserialization
+        ObjectInputStream objectInputStream =
+                new ObjectInputStream(new FileInputStream("abc.txt"));
+        Car1 car1 = (Car1) objectInputStream.readObject();
+
+        System.out.println(car1.name);
+        System.out.println(car1.color);
+    }
+}
