1c3f22ca7d7e9a76230e8344cbe2d63f601b37a8
Swan Htet Aung <137768986+swanhtetaung01@users.noreply.github.com>
2025-12-31 17:49:48 +0700

    Add inheritance example for serialization

---
+++ src/main/java/org/javarevision/FileHandlingUsingJavaIOapi/SerializationAndDeserialization/InheritanceSerializationDemo.java
@@ -0,0 +1,45 @@
+package org.javarevision.FileHandlingUsingJavaIOapi.SerializationAndDeserialization;
+
+import java.io.*;
+
+class CarInheritance{
+    String name;
+    String color;
+
+    public CarInheritance() {
+    }
+
+    public CarInheritance(String name, String color) {
+        this.name = name;
+        this.color = color;
+    }
+}
+
+class Ferrari extends CarInheritance implements Serializable{
+    int wheels;
+
+    public Ferrari(String name, String color, int wheels) {
+        super(name, color);
+        this.wheels = wheels;
+    }
+}
+
+public class InheritanceSerializationDemo {
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        Ferrari ferrari = new Ferrari("Ferrari", "Red", 4);
+
+        //Serialization
+        ObjectOutputStream objectOutputStream =
+                new ObjectOutputStream(new FileOutputStream("abc.txt"));
+        objectOutputStream.writeObject(ferrari);
+
+        //Deserialization
+        ObjectInputStream objectInputStream =
+                new ObjectInputStream(new FileInputStream("abc.txt"));
+        Ferrari ferrari1 = (Ferrari) objectInputStream.readObject();
+
+        System.out.println(ferrari1.color);
+        System.out.println(ferrari1.name);
+        System.out.println(ferrari1.wheels);
+    }
+}
