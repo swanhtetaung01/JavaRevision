f2cd22b0aac9de4db9c82d8bb7501918c3a7746c
Swan Htet Aung <137768986+swanhtetaung01@users.noreply.github.com>
2026-01-02 23:47:03 +0700

    Update Thread Package

---
+++ src/main/java/org/javarevision/Thread/ExecutorServiceExample.java
@@ -0,0 +1,13 @@
+package org.javarevision.Thread;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class ExecutorServiceExample {
+    public static void main(String[] args) {
+        ExecutorService executorService = Executors.newFixedThreadPool(4);
+        for (int i = 0; i < 10; i++) {
+            executorService.execute(new Task());
+        }
+    }
+}
