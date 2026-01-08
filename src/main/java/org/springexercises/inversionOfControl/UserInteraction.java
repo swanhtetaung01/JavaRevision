1d1eb023e789aae671562e194657355c3eb10e16
Swan Htet Aung <137768986+swanhtetaung01@users.noreply.github.com>
2026-01-08 23:39:24 +0700

    Update IOC exercise

--- src/main/java/org/springexercises/inversionOfControl/UserInteraction.java
+++ src/main/java/org/springexercises/inversionOfControl/UserInteraction.java
@@ -8,9 +8,21 @@ public class UserInteraction {
     public static void main(String[] args) {
         ApplicationContext context
                 = new ClassPathXmlApplicationContext("applicationInversionOfControl.xml");
-        UserManager usermanager = (UserManager) context.getBean("usermanager");
-        System.out.println(usermanager.getUserDetailsFromDB());
-        System.out.println(usermanager.getUserDetailsFromWeb());
-        System.out.println(usermanager.getUserDetailsFromLocal());
+        UserManager usermanagerdb = (UserManager) context.getBean("usermanagerDB");
+        System.out.println(usermanagerdb.getUserDetailsFromDB());
+
+        UserManager usermanagerweb = (UserManager) context.getBean("usermanagerWeb");
+        System.out.println(usermanagerweb.getUserDetailsFromWeb());
+
+        UserManager usermanagerlocal = (UserManager) context.getBean("usermanagerLocal");
+        System.out.println(usermanagerlocal.getUserDetailsFromLocal());
+
+        System.out.println("///////////////////////////////");
+        System.out.println("Three args constructor : ");
+
+        UserManager userManager = (UserManager) context.getBean("usermanager");
+        System.out.println(userManager.getUserDetailsFromDB());
+        System.out.println(userManager.getUserDetailsFromWeb());
+        System.out.println(userManager.getUserDetailsFromLocal());
     }
 }
